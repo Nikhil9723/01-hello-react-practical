85aceaace4dccc4b96cfb703f2e5259cc73eedb2
Nikhil Prajapati <nikhil.prajapati@simformsolutions.com>
2025-04-25 13:13:36 +0530

    SCAFFOLD: add name, email, and github account into app.jsx and add required stylings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 function App() {
   return (
-    <div className="App">
+    <>
+      <h1>Hello React</h1>
+      <h2>Name: Nikhil Kiritbhai Prajapati</h2>
+      <p>Hii,my name is Nikhil and I am  React traniee at simform</p>
+      <p>Email: nikhil.prajapati@simformsolutions.com</p>
+      <p>
+        Github ID:
+        <a href="https://github.com/Nikhil9723">https://github.com/Nikhil9723</a>
+      </p>
       
-    </div>
+    </>
   );
 }
 
