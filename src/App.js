e5f5fca5a9188ccf15fd33e8ee76f7b6fe320d44
Nikhil Prajapati <nikhil.prajapati@simformsolutions.com>
2025-05-06 17:07:21 +0530

    added mailto attribute to mail address

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ function App() {
       <h1>Hello React</h1>
       <h2>Name: Nikhil Kiritbhai Prajapati</h2>
       <p>Hii,my name is Nikhil and I am  React traniee at simform</p>
-      <p>Email: nikhil.prajapati@simformsolutions.com</p>
+      <a href="mailto: nikhil.prajapati@simform.dom">Email: nikhil.prajapati@simformsolutions.com</a>
       <p>
         Github ID:
-        <a href="mailto:https://github.com/Nikhil9723">https://github.com/Nikhil9723</a>
+        <a href="https://github.com/Nikhil9723">https://github.com/Nikhil9723</a>
       </p>
       
     </>
